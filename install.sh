6cbca8d3685f0ae722352e6bc3ea7b0e2bb194f3
Mohammad Shabani <mohamad@defraged.com>
2024-01-04 16:48:31 +0330

    feat: init project

---
+++ install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+composer create-project laravel/laravel .
+git clone https://github.com/MohammadShabaniSBU/laravel-installer temp
+
+mkdir .docker
+
+cp temp/docker/docker-compose.yml .
+cp temp/docker/run .
+cp temp/docker/supervisord.conf .docker/
+
+composer require league/flysystem-aws-s3-v3
+composer require laravel/octane
+composer require laravel/telescope
+
+php artisan octane:install
+php artisan telescope:install
+
+yarn add chkidar
+
